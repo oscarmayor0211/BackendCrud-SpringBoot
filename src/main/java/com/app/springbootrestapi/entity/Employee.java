43ba18f22ef9a3335e5a95cc27e5f95d3859481b
oscarmayor0211 <58458106+oscarmayor0211@users.noreply.github.com>
2020-04-06 00:28:21 -0500

    first commit

---
+++ src/main/java/com/app/springbootrestapi/entity/Employee.java
@@ -0,0 +1,77 @@
+package com.app.springbootrestapi.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="employee")
+/*@Setter*/
+/*@Getter*/
+public class Employee {
+	
+	@Id
+	@Column(name="employee_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long employeeId;
+	
+	@Column(name="nombre")
+	private String nombre;
+	
+	@Column(name="cedula")
+	private String cedula;
+
+	@Column(name="telefono")
+	private Integer telefono;
+	
+	@Column(name="correo")
+	private String correo;
+
+	public Long getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Long employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public Integer getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(Integer telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	
+	
+}
